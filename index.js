174f72563d173d14f19ddc861907c5f3537c31aa
Velazco <velazcof@cbh.com.au>
2024-01-08 15:49:21 +0800

    Initial commit with layout of page

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log("Listening to port " + port)
+});
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/about/", (req, res) => {
+    res.render("about.ejs");
+});
