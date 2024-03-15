064055a9c6de7933ac4a4a132504f6a716d9fcea
FranDiGio <francodvf06@gmail.com>
2024-03-15 22:31:22 +0800

    Implement trending button event for feed

--- public/js/trending.js
+++ public/js/trending.js
@@ -2,12 +2,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     var rightPanel = document.getElementById('fixed-sidebar-right');
     var leftPanel = document.getElementById('fixed-sidebar-left');
+    var postsTitle = document.getElementById("posts-title");
+    var creatorsTitle = document.getElementById("creators-title");
+    var trendButton = document.getElementById("trending-btn");
+    var isTrending = false;
 
     function ShowTrending() {
+        postsTitle.innerText = "Trending";
+        creatorsTitle.innerText = "Top Creators";
     }
 
     function ShowLocal() {
-
+        postsTitle.innerText = "My Posts";
+        creatorsTitle.innerText = "Following";
     }
+
+    function toggleTrending() {
+        if (isTrending) {
+            ShowLocal();
+            isTrending = false;
+        } else {
+            ShowTrending();
+            isTrending = true;
+        }
+    }
+
+    trendButton.addEventListener('click', toggleTrending);
 });
 
