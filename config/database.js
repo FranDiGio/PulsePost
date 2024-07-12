edb2ec10a9d9523cd1c164253e0e71df011af1bc
FranDiGio <francodvf06@gmail.com>
2024-07-12 14:34:32 +0800

    New background for contact page and add firebase config

--- config/database.js
+++
@@ -1,6 +0,0 @@
-import firebaseApp from "firebaseConfig"
-import { getDatabase } from "firebase/database"
-
-const database = getDatabase(firebaseApp);
-
-export default database;
--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
 import session from "express-session";
+import dotenv from 'dotenv';
 import { isStrongPassword } from "./services/userService.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { User } from './models/User.mjs';
-import database from "config/database";
-import { ref, set } from firebase/database;
+import { getDatabase, ref, set } from "firebase/database";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 let users = [];
@@ -21,10 +37,10 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: { secure: 'auto' }
-  }));
-  
+}));
+
 app.listen(port, () => {
-    console.log("Listening to port " + port)
+    console.log("Listening to port " + port);
 });
 
 app.get("/", (req, res) => {
@@ -53,20 +69,26 @@ app.get("/login/", (req, res) => {
 
 app.post('/api/signup', (req, res) => {
     const { nickname, email, password } = req.body;
-    
-    if (users.some(user => user.nickname === nickname)){
+
+    if (users.some(user => user.nickname === nickname)) {
         res.render("sign-up.ejs", { success: false, invalidNickname: true });
     } 
-    else if (users.some(user => user.email === email)){
+    else if (users.some(user => user.email === email)) {
         res.render("sign-up.ejs", { success: false, invalidEmail: true });
-    }
-    else if (!isStrongPassword(password)){
+    } 
+    else if (!isStrongPassword(password)) {
         res.render("sign-up.ejs", { success: false, invalidPassword: true });
-    }
+    } 
     else {
         const newUser = new User(nickname, email, password);
         users.push(newUser);
         req.session.nickname = newUser.nickname;
+        
+        set(ref(database, 'users/' + newUser.nickname), {
+            nickname: newUser.nickname,
+            email: newUser.email
+        });
+        
         res.render("sign-up.ejs", { success: true });
     }
 });
@@ -75,9 +97,9 @@ app.post('/api/login', (req, res) => {
     const { email, password } = req.body;
     const userExists = users.some(user => user.email === email && user.password === password);
 
-    if (userExists){
+    if (userExists) {
         res.redirect("/feed/");
     } else {
-        res.render("log-in.ejs", { invalid: true })
+        res.render("log-in.ejs", { invalid: true });
     }
 });
