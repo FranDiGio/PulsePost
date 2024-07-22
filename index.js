f6a87f5eca70fb100c185c9a02c76b1ee83372d6
FranDiGio <francodvf06@gmail.com>
2024-07-22 15:55:34 +0800

    Add database initialization into separate module

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import session from 'express-session';
-import dotenv from 'dotenv';
 import { isStrongPassword } from './services/userService.js';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { User } from './models/User.mjs';
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, set, push, query, orderByChild, get, equalTo } from 'firebase/database';
-
-dotenv.config();
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getDatabase(firebaseApp);
+import { ref, set, push, query, orderByChild, get, equalTo } from 'firebase/database';
+import { db } from './config/firebaseConfig.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -71,7 +54,7 @@ app.get('/login/', (req, res) => {
 app.post('/api/signup', async (req, res) => {
     const { username, email, password } = req.body;
     const userRef = ref(db, 'users');
-    let invalidField = "none";
+    let invalidField = [];
 
     // Duplicate username check-up
     const usernameSnapshot = await get(query(userRef, orderByChild('username'), equalTo(username)));
--- services/userService.js
+++ services/userService.js
@@ -1,5 +1,9 @@
 import validator from "validator";
 
+// function validateSignUp(res, db) {
+
+// }
+
 export function isStrongPassword(password) {
     return validator.isStrongPassword(password, {
         minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1
