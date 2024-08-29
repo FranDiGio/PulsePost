df0086f42197c69ff86480e9bddd0ede7aeda3bf
FranDiGio <francodvf06@gmail.com>
2024-08-29 17:08:23 +0800

    Add "View Profile" user-tag functionality and its dedicated route

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => (req.session.username ? res.redirect('/feed/') : res.render('index.ejs')))
 router.get('/feed/', ensureAuthenticated, (req, res) => res.render('feed.ejs', { username: req.session.username }))
+router.get('/profile/:username', (req, res) => res.render('profile.ejs', { username: req.params.username }))
 router.get('/about/', (req, res) => res.render('about.ejs'))
 router.get('/contact/', (req, res) => res.render('contact.ejs'))
 router.get('/signup/', (req, res) =>
