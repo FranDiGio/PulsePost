8385cebdf001e15891ead9cd7d061e95d15e89a4
FranDiGio <francodvf06@gmail.com>
2024-08-29 17:07:00 +0800

    Merge js files functionality and rename files

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,3 +1,9 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"].is-invalid'))
+
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
 gsap.from('.fixed-sidebar-left', {
     duration: 1,
     x: -100,
