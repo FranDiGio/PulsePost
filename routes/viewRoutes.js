41c8845d33570ba80e40fedf2a7ca93274e9bd84
FranDiGio <francodvf06@gmail.com>
2024-08-29 11:36:48 +0800

    Redirect signed-in users to feed page when accessing the main route

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,12 +3,17 @@ import { ensureAuthenticated } from '../services/authService.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => res.render('index.ejs'))
+router.get('/', (req, res) => (req.session.username ? res.redirect('/feed/') : res.render('index.ejs')))
 router.get('/feed/', ensureAuthenticated, (req, res) => res.render('feed.ejs', { username: req.session.username }))
 router.get('/about/', (req, res) => res.render('about.ejs'))
 router.get('/contact/', (req, res) => res.render('contact.ejs'))
 router.get('/signup/', (req, res) =>
-    res.render('sign-up.ejs', { success: false, invalidUsername: false, invalidEmail: false, invalidPassword: false })
+    res.render('sign-up.ejs', {
+        success: false,
+        invalidUsername: false,
+        invalidEmail: false,
+        invalidPassword: false,
+    })
 )
 router.get('/login/', (req, res) => res.render('log-in.ejs', { invalidCredentials: false }))
 
