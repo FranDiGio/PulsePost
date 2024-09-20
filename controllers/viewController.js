74b55fbd3ab60824e032aca5d777482b758d6e5f
FranDiGio <francodvf06@gmail.com>
2024-09-20 23:56:22 +0800

    Add profile picture loading to profile page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -3,17 +3,7 @@ import { db } from '../config/firebaseConfig.js'
 
 export async function loadFeed(req, res) {
     try {
-        const userRef = ref(db, `users/${req.session.userId}`)
-        const userSnapshot = await get(userRef)
-        const userData = userSnapshot.val()
-
-        let profilePictureUrl
-
-        if (userData && userData.profilePicture && userData.profilePicture !== 'N/A') {
-            profilePictureUrl = userData.profilePicture
-        } else {
-            profilePictureUrl = '/images/default-profile.png'
-        }
+        const profilePictureUrl = await getProfilePictureUrl(req.session.userId)
 
         res.render('feed.ejs', {
             username: req.session.username,
@@ -22,5 +12,36 @@ export async function loadFeed(req, res) {
     } catch (error) {
         console.error('Error fetching user data:', error)
         res.redirect('/login')
+    }
+}
+
+export async function loadProfile(req, res) {
+    try {
+        const profilePictureUrl = await getProfilePictureUrl(req.session.userId)
+
+        res.render('profile.ejs', { 
+            username: req.params.username,
+            profilePictureUrl: profilePictureUrl,
+        })
+    } catch (error) {
+        console.error('Error fetching user data:', error)
+        res.redirect('/login')
+    }
+}
+
+async function getProfilePictureUrl(userId) {
+    try {
+        const userRef = ref(db, `users/${userId}`)
+        const userSnapshot = await get(userRef)
+        const userData = userSnapshot.val()
+
+        if (userData && userData.profilePicture && userData.profilePicture !== 'N/A') {
+            return userData.profilePicture
+        } else {
+            return '/images/default-profile.png'
+        }
+    } catch (error) {
+        console.error('Error fetching user data:', error)
+        return '/images/default-profile.png'
     }
 }
