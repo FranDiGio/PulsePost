aa0877d7f72c72875930cc0e82cfaed427803c75
FranDiGio <francodvf06@gmail.com>
2024-10-30 14:55:06 +0800

    Fix warning of service account key json import

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,9 +3,22 @@ import admin from 'firebase-admin';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
-import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
 
 dotenv.config();
+
+const serviceAccount = {
+	type: process.env.FIREBASE_TYPE,
+	project_id: process.env.FIREBASE_PROJECT_ID,
+	private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+	private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+	client_email: process.env.FIREBASE_CLIENT_EMAIL,
+	client_id: process.env.FIREBASE_CLIENT_ID,
+	auth_uri: process.env.FIREBASE_AUTH_URI,
+	token_uri: process.env.FIREBASE_TOKEN_URI,
+	auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+	client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+	universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 const firebaseConfig = {
 	apiKey: process.env.FIREBASE_API_KEY,
