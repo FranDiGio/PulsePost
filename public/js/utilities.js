f3ce3065042d4c53840e90b601a94f1f979d895e
FranDiGio <francodvf06@gmail.com>
2024-10-31 15:23:54 +0800

    Fix formatting of break lines when displaying posts

---
+++ public/js/utilities.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+	// Set like button animation
+	document.getElementById('like-button').addEventListener('click', function () {
+		const heartIcon = this.querySelector('.heart-icon');
+		heartIcon.classList.toggle('filled');
+	});
+
+	// Keep post content break lines
+	const postContents = document.querySelectorAll('.post-content');
+	postContents.forEach(function (postContent) {
+		const text = postContent.textContent;
+		const formattedText = text.replace(/\n/g, '<br>');
+		postContent.innerHTML = formattedText;
+	});
+
+	// Initialize character counter for content of post and bio
+	function updateCharCount(textareaId, charCountId, maxChars) {
+		const textarea = document.getElementById(textareaId);
+		const charCount = document.getElementById(charCountId);
+
+		textarea.addEventListener('input', () => {
+			const remaining = maxChars - textarea.value.length;
+			charCount.textContent = remaining;
+		});
+	}
+	updateCharCount('postContent', 'postCharCount', 1500);
+});
