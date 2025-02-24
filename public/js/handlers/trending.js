c4b3d3ae6274943469f396d65e127a2779271f71
FranDiGio <francodvf06@gmail.com>
2025-02-24 11:26:20 +0800

    Add trending mode animations

--- public/js/handlers/trending.js
+++ public/js/handlers/trending.js
@@ -1,30 +1,56 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var rightPanel = document.getElementById('fixed-sidebar-right');
-	var leftPanel = document.getElementById('fixed-sidebar-left');
-	var postsTitle = document.getElementById('posts-title');
-	var creatorsTitle = document.getElementById('creators-title');
-	var trendButton = document.getElementById('trending-btn');
+	const rightPanel = document.querySelector('.fixed-sidebar-right');
+	const leftPanel = document.querySelector('.fixed-sidebar-left');
+	const postsTitle = document.getElementById('posts-title');
+	const creatorsTitle = document.getElementById('creators-title');
+	const trendButton = document.getElementById('trending-btn');
+	const trendImage = document.getElementById('trending-img');
 	var isTrending = false;
 
-	function ShowTrending() {
-		postsTitle.innerText = 'Trending';
-		creatorsTitle.innerText = 'Top Creators';
-	}
-
-	function ShowLocal() {
-		postsTitle.innerText = 'My Posts';
-		creatorsTitle.innerText = 'Following';
-	}
-
 	function toggleTrending() {
-		if (isTrending) {
-			ShowLocal();
-			isTrending = false;
+		if (leftPanel && rightPanel && postsTitle && creatorsTitle && trendImage) {
+			if (isTrending) {
+				trendImage.src = '/svg/fire.svg';
+			} else {
+				trendImage.src = '/svg/fire-lit.svg';
+			}
+
+			// Slide out the sidebars
+			leftPanel.classList.remove('slide-in-left');
+			leftPanel.classList.add('slide-out-left');
+			rightPanel.classList.remove('slide-in-right');
+			rightPanel.classList.add('slide-out-right');
+
+			// Hide the sidebars after the slide-out animation
+			setTimeout(() => {
+				leftPanel.classList.add('hidden');
+				rightPanel.classList.add('hidden');
+
+				// Update content
+				if (isTrending) {
+					postsTitle.innerText = 'My Posts';
+					creatorsTitle.innerText = 'Following';
+					isTrending = false;
+				} else {
+					postsTitle.innerText = 'Trending';
+					creatorsTitle.innerText = 'Top Creators';
+					isTrending = true;
+				}
+
+				// Show the sidebars with slide-in animation
+				leftPanel.classList.remove('hidden', 'slide-out-left');
+				leftPanel.classList.add('slide-in-left');
+				rightPanel.classList.remove('hidden', 'slide-out-right');
+				rightPanel.classList.add('slide-in-right');
+			}, 500); // Match the duration of the slide-out animation
 		} else {
-			ShowTrending();
-			isTrending = true;
+			console.error('One or more elements are not found.');
 		}
 	}
 
-	trendButton.addEventListener('click', toggleTrending);
+	if (trendButton) {
+		trendButton.addEventListener('click', toggleTrending);
+	} else {
+		console.error('Trending button not found.');
+	}
 });
